cc237f2cdb68d64afa41fce2f9efcc2ae83f0e0f
kaviska <126580314+kaviska@users.noreply.github.com>
2026-01-23 21:30:51 +0530

    Add .htaccess for security and update asset versioning
    Introduced a new .htaccess file to improve basic security, enable gzip compression, and set cache headers. Updated all PHP and component files to append '?v=3' to CSS and JS asset URLs for cache busting and version control.

--- index.php
+++ index.php
@@ -21,25 +21,25 @@
     <link rel="apple-touch-icon" href="assets/app-icons/icon-180x180.png">
 
     <!-- Theme switcher (color modes) -->
-    <script src="assets/js/theme-switcher.js"></script>
+    <script src="assets/js/theme-switcher.js?v=3"></script>
 
     <!-- Preloaded local web font (Inter) -->
     <link rel="preload" href="assets/fonts/inter-variable-latin.woff2" as="font" type="font/woff2" crossorigin>
 
     <!-- Font icons -->
     <link rel="preload" href="assets/icons/cartzilla-icons.woff2" as="font" type="font/woff2" crossorigin>
-    <link rel="stylesheet" href="assets/icons/cartzilla-icons.min.css">
+    <link rel="stylesheet" href="assets/icons/cartzilla-icons.min.css?v=3">
 
     <!-- Vendor styles -->
-    <link rel="stylesheet" href="assets/vendor/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/vendor/swiper/swiper-bundle.min.css?v=3">
 
     <!-- Bootstrap + Theme styles -->
-    <link rel="preload" href="assets/css/theme.min.css" as="style">
-    <link rel="preload" href="assets/css/theme.rtl.min.css" as="style">
-    <link rel="stylesheet" href="assets/css/theme.min.css" id="theme-styles">
+    <link rel="preload" href="assets/css/theme.min.css?v=3" as="style">
+    <link rel="preload" href="assets/css/theme.rtl.min.css?v=3" as="style">
+    <link rel="stylesheet" href="assets/css/theme.min.css?v=3" id="theme-styles">
 
     <!-- custome script -->
-    <script defer src="src/js/custom/main.js"></script>
+    <script defer src="src/js/custom/main.js?v=3"></script>
   </head>
 
 
@@ -386,9 +386,9 @@
 
 
     <!-- Vendor scripts -->
-    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js?v=3"></script>
 
     <!-- Bootstrap + Theme scripts -->
-    <script src="assets/js/theme.min.js"></script>
+    <script src="assets/js/theme.min.js?v=3"></script>
   </body>
 </html>
